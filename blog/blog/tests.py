4bde15cfd2d8e9a8d2d59c3761a2e782d778674d
Sanni Habeebullahi <hsanni14@yahoo.com>
2021-04-23 18:39:56 +0100

    blog-with-Heroku-config

--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -24,6 +24,8 @@ class BlogTests(TestCase):
     def test_string_representation(self):
         Post= post(title='A simple title')
         self.assertEqual(str(Post), Post.title)
+    def test_get_absolute_url(self): # new
+        self.assertEqual(self.Post.get_absolute_url(), '/post/1/')
 
     def test_post_content(self):
         self.assertEqual(f'{self.Post.title}', 'A good title')
@@ -43,3 +45,23 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self): # new
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(post.objects.last().title, 'New title')
+        self.assertEqual(post.objects.last().body, 'New text')
+    def test_post_update_view(self): # new
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+    def test_post_delete_view(self): # new
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
